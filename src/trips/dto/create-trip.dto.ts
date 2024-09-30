cf8e08e62f1f332660883e8ec3f237f7eba0d63e
윤재형 <yoonwogud@gmail.com>
2024-09-30 22:41:48 +0900

    expense, trip 옮기기 끝

---
+++ src/trips/dto/create-trip.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, IsDateString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTripDto {
+  @ApiProperty({ description: 'string', example: '여행 이름' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ description: 'string', example: '여행 장소' })
+  @IsString()
+  location: string;
+
+  @ApiProperty({ description: '여행 시작 날짜', example: 'yyyy-mm-dd' })
+  @IsDateString()
+  start_date: Date;
+
+  @ApiProperty({ description: '여행 종료 날짜', example: 'yyyy-mm-dd' })
+  @IsDateString()
+  end_date: Date;
+}
