d6423874aec673c20e4389e461957e1cc42749cd
윤재형 <yoonwogud@gmail.com>
2024-10-07 17:31:40 +0900

    주석 삭제

--- src/participants/participant.entity.ts
+++ src/participants/participant.entity.ts
@@ -11,33 +11,11 @@ import { Trip } from 'src/trips/trip.entity';
 
 @Entity('participants')
 export class Participants {
-  // @PrimaryGeneratedColumn()
-  // id: number;
-
-  // @ManyToOne(() => Trip, (trip) => trip.participants, { onDelete: 'CASCADE' })
-  // @JoinColumn({ name: 'trip_id' })
-  // trip: Trip;
-
-  // @ManyToOne(() => Member, (member) => member.participations, {
-  //   onDelete: 'CASCADE',
-  // })
-  // @JoinColumn({ name: 'member_id' })
-  // member: Member; // 회원 인덱스
-
-  // @Column()
-  // room_number: number; // 방 번호를 저장하는 컬럼
-
-  // @Column()
-  // userid: string; // userid , 닉네임 columns
-
-
-
+  @PrimaryColumn()
+  trip_id: number; // 기본키의 일부로 사용
 
   @PrimaryColumn()
-  trip_id: number;  // 기본키의 일부로 사용
-
-  @PrimaryColumn()
-  member_id: number;  // 기본키의 일부로 사용
+  member_id: number; // 기본키의 일부로 사용
 
   @ManyToOne(() => Trip, (trip) => trip.participants, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'trip_id' })
@@ -53,5 +31,5 @@ export class Participants {
   // room_number: number;  // 방 번호
 
   @Column()
-  userid: string;  // 사용자 ID 또는 닉네임
+  userid: string; // 사용자 ID 또는 닉네임
 }
